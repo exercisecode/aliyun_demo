1be8a5b76684d349a39e2d6b0af3905c51014e52
李东旭 <lidongxu@lidongxus-iMac.local>
2016-07-28 20:05:43 +0800

    2016-07-28 lidongxu 阿里云MNS测试Queue收发消息及删除.

--- aliyun/src/main/java/com/demo/aliyun/mns/AliyunMNSQueueService.java
+++ aliyun/src/main/java/com/demo/aliyun/mns/AliyunMNSQueueService.java
@@ -7,32 +7,104 @@ import org.springframework.stereotype.Service;
 
 import com.aliyun.mns.client.CloudQueue;
 import com.aliyun.mns.client.MNSClient;
+import com.aliyun.mns.model.Message;
 import com.aliyun.mns.model.QueueMeta;
 
 @Service
 public class AliyunMNSQueueService {
-	
-	static Logger log = LoggerFactory.getLogger(AliyunMNSQueueService.class);
 
-	private static String QUEUE_NAME = "lidongxuDemoQuery";
+	static Logger log = LoggerFactory.getLogger(AliyunMNSQueueService.class);
 
 	@Autowired
 	private MNSClient mnsClient;
 
-	public void createQueue() {
+	public void createQueue(String queueName) {
 		try {
 			QueueMeta queueMeta = new QueueMeta();
-			queueMeta.setQueueName(QUEUE_NAME);
+			queueMeta.setQueueName(queueName);
 			queueMeta.setPollingWaitSeconds(15);
 			queueMeta.setMaxMessageSize(1024 * 10L);
 
 			CloudQueue queue = mnsClient.createQueue(queueMeta);
-			log.info("-----createQuery name:{} CloudQuery:{} ", QUEUE_NAME, queue);
-			
+			log.info("-----createQuery name:{} CloudQuery:{} ", queueName, queue);
+
 		} catch (Exception e) {
 			MNSExceptionHandler.deal(e);
 		}
 
+	}
+
+	public String sendQueueMessage(String queueName, String message) {
+		String messageId = null;
+		try {
+			CloudQueue cloudQueue = mnsClient.getQueueRef(queueName);
+			if (cloudQueue != null) {
+				Message sendMessage = new Message();
+				sendMessage.setMessageBody(message);
+
+				Message afterSendMessage = cloudQueue.putMessage(sendMessage);
+				if (afterSendMessage != null) {
+					messageId = afterSendMessage.getMessageId();
+				}
+				log.info("-----sendQueueMessage after send messageId:{} afterSendMessage:{} ", messageId,
+						afterSendMessage);
+			} else {
+				log.warn("-----sendQueueMessage getQueueRef queueName:{} return null. ", queueName);
+			}
+		} catch (Exception e) {
+			MNSExceptionHandler.deal(e);
+		}
+
+		return messageId;
+	}
+
+	public Message popQueueMessage(String queueName) {
+		Message popMessage = null;
+		try {
+			CloudQueue cloudQueue = mnsClient.getQueueRef(queueName);
+			popMessage = cloudQueue.popMessage();
+			if (popMessage != null) {
+				log.info(
+						"-----popQueueMessage queueName:{} messageId:{} messageReceipHandle:{} messageDequeueCount:{} messageBody:{} ",
+						queueName, popMessage.getMessageId(), popMessage.getReceiptHandle(),
+						popMessage.getDequeueCount(), popMessage.getMessageBodyAsString());
+			} else {
+				log.info("-----popQueueMessage queueName:{} no message. ", queueName);
+			}
+		} catch (Exception e) {
+			MNSExceptionHandler.deal(e);
+		}
+		return popMessage;
+	}
+
+	public void deleteQueueMessage(String queueName, String receiptHandle) {
+		try {
+			CloudQueue cloudQueue = mnsClient.getQueueRef(queueName);
+			if (cloudQueue != null) {
+				cloudQueue.deleteMessage(receiptHandle);
+				log.info("-----deleteQueueMessage queueName:{} deleteMessage receiptHandle:{} ", queueName,
+						receiptHandle);
+			} else {
+				log.info("-----deleteQueueMessage queueName:{} not exists ", queueName);
+			}
+		} catch (Exception e) {
+			MNSExceptionHandler.deal(e);
+		}
+	}
+
+	public void deleteQueue(String queueName) {
+		try {
+			CloudQueue cloudQueue = mnsClient.getQueueRef(queueName);
+			if (cloudQueue != null) {
+				cloudQueue.delete();
+				log.info("-----deleteQueue queueName:{}  ", queueName);
+			} else {
+				log.info("-----deleteQueue queueName:{} not exists ", queueName);
+			}
+
+		} catch (Exception e) {
+			MNSExceptionHandler.deal(e);
+		}
 	}
 
 }
--- aliyun/src/test/java/com/demo/aliyun/mns/AliyunMNSQueueServiceTest.java
+++ aliyun/src/test/java/com/demo/aliyun/mns/AliyunMNSQueueServiceTest.java
@@ -1,26 +1,63 @@
 package com.demo.aliyun.mns;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.aliyun.mns.model.Message;
 import com.demo.aliyun.AliyunDemoMain;
 
 import junit.framework.TestCase;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(AliyunDemoMain.class)
 public class AliyunMNSQueueServiceTest extends TestCase {
-	
+
+	public static String QUEUE_NAME = "lidongxuDemoQuery";
+
 	@Autowired
 	private AliyunMNSQueueService aliyunMNSQueueService;
-	
+
 	@Test
-	public void testCreateQuery(){
-		aliyunMNSQueueService.createQueue();
+	public void testFull() {
+		// create queue
+		aliyunMNSQueueService.createQueue(QUEUE_NAME);
+
+		// send message
+		// com.aliyun.mns.common.ServiceException: The queue name you provided
+		// is not exist
+		// aliyunMNSQueueService.sendQueueMessage("demo", "不存在的queue");
+		String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
+		aliyunMNSQueueService.sendQueueMessage(QUEUE_NAME, "lidongxu test数据信息" + timeStamp);
+
+		List<String> receiptHandleList = new ArrayList<String>();
+		// pop message
+		Message popMessage = aliyunMNSQueueService.popQueueMessage(QUEUE_NAME);
+		if (popMessage != null && popMessage.getReceiptHandle() != null) {
+			receiptHandleList.add(popMessage.getReceiptHandle());
+		}
+		while (popMessage != null) {
+			popMessage = aliyunMNSQueueService.popQueueMessage(QUEUE_NAME);
+			if (popMessage != null && popMessage.getReceiptHandle() != null) {
+				receiptHandleList.add(popMessage.getReceiptHandle());
+			}
+		}
+
+		// delete message
+		for (String receiptHandle : receiptHandleList) {
+			aliyunMNSQueueService.deleteQueueMessage(QUEUE_NAME, receiptHandle);
+		}
+
+		// delete queue
+		aliyunMNSQueueService.deleteQueue(QUEUE_NAME);
+
 	}
 
 }
